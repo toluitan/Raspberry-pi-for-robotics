00283d33e7c5f6bffd7d6924f8b6b9f5989de418
toluitan <toluitan@gmail>
2020-12-08 14:47:52 +0000

    made changes to the pySerial_Distance.py code to properly decode data sent from arduino

--- pySerial_Distance.py
+++ pySerial_Distance.py
@@ -1,13 +1,15 @@
 import serial
-arduino = serial.Serial('/dev/ttyUSB1', 9600)
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
 while True:
+    
     data = arduino.readline().decode('ascii') #the last bit gets rid of the new-line
+    
     if data:
-        data = data.split("\r")
+        data = data.split()
         data = int(data[0])
-        if data:
-            print(data)
-        if data < 15:
+        print("distance to obstacle is: ",data)
+        
+        if data < 20:
             
             arduino.write(b'H')
             
